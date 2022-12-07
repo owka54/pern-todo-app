b505da9ad2b609923152edb4e509e4304945276e
Owen <owen.kane54@gmail.com>
2022-12-07 10:07:41 +0000

    Add create todo & get todos routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,43 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
-//middleware
+// middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
+
+// ROUTES //
+
+// create a todo
+
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", [description]);
+
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// get all todos
+
+app.get("/todos", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// get a todo
+
+// update a todo
+
+// delete a todo
 
 
 app.listen(5000, () => {
