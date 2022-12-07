7698a6467fdd1d83d81b937169bdeef6de6d08c1
Owen <owen.kane54@gmail.com>
2022-12-07 11:05:04 +0000

    Add input todo component

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+// components
+
+import InputTodo from "./components/InputTodo";
+
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
